82eb94791c9f81092f7dd33a18e4933a1a6684c8
Henry Laynesa Jr <henry.laynesa@gmail.com>
2020-04-27 14:10:41 +0800

    Changed setup.py to use setup.cfg

--- setup.py
+++ setup.py
@@ -1,17 +1,3 @@
-import sys
-from distutils.core import setup
+from setuptools import setup
 
-from setuptools import find_packages
-
-install_requires = ["black==19.10b0,<20", "isort>=4.1.1,<5"]
-if sys.version_info < (2, 7):
-    install_requires.append("argparse")
-
-setup(
-    name="Black Notebook",
-    version="0.0.1",
-    packages=find_packages(),
-    entry_points={
-        "console_scripts": ["black_notebook = jupyter_precommit.black_notebook:main"]
-    },
-)
+setup()
